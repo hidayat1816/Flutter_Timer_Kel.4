0fb0dabd5df66611c955d564cc1504be5e84e10f
julitaliita <julitajulita715@gmail.com>
2026-05-10 22:30:12 -0700

    Update ticker.dart

--- lib/ticker.dart
+++ lib/ticker.dart
@@ -1,8 +1,7 @@
 class Ticker {
-  const Ticker();
   Stream<int> tick({required int ticks}) {
     return Stream.periodic(
-      const Duration(seconds: 1),
+      const Duration(milliseconds: 100),
       (x) => ticks - x - 1,
     ).take(ticks);
   }
