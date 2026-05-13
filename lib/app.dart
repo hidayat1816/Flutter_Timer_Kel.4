e981c937fbc80cfac7bf730bc964c252f4affd63
julitaliita <julitajulita715@gmail.com>
2026-05-12 23:13:22 -0700

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -9,11 +9,27 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Timer',
+
       theme: ThemeData(
+        brightness: Brightness.light,
+
+        scaffoldBackgroundColor: const Color(0xFFF5F7FB),
+
         colorScheme: const ColorScheme.light(
           primary: Color.fromRGBO(72, 74, 126, 1),
         ),
+
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          elevation: 0,
+        ),
+
+        iconTheme: const IconThemeData(
+          color: Colors.black87,
+        ),
       ),
+
       home: const TimerPage(),
     );
   }
